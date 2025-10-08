b29cc151660fcb57ebe9198db84695e56c687128
Fabricio valdivia <walter.valdivia7685242@gmail.com>
2025-10-08 01:38:13 -0500

    proyecto de terraform EKS alta disponibilidad v2

--- versions.tf
+++ versions.tf
@@ -11,3 +11,9 @@ provider "aws" {
   region  = "us-east-1"
   profile = "credencialfabri"
 }
+
+#Terraform usa las credenciales guardadas en tu máquina, en un archivo llamado ~/.aws/credentials (Linux/Mac) o en C:\Users\<tu_usuario>\.aws\credentials (Windows). Para crear ese perfil, ejecuta en tu terminal:
+#aws configure --profile credencialfabri
+#Buscará las credenciales en tu perfil credencialfabri.
+#Usará la región us-east-1.
+#Autenticará las llamadas a AWS sin que pongas tus claves dentro del código (⚠️ lo que es mucho más seguro).
